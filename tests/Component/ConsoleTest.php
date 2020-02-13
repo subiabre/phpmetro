7252db4fa216271b8018f6b13dde788cbcb8c79d
Facu <subiabrewd@gmail.com>
2020-02-13 10:49:19 +0100

    Add Console component tests

---
+++ tests/Component/ConsoleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PHPMetro\Tests\Component;
+
+use PHPUnit\Framework\TestCase;
+use PHPMetro\Component\Console;
+
+class ConsoleTest extends TestCase
+{
+    public $console;
+
+    public function setUp(): void
+    {
+        $this->console = new Console;
+    }
+
+    public function testConsoleTakesInputFromCLI()
+    {
+        $input = $this->console->read('Please say something for the test:');
+
+        $this->assertEquals(
+            $input,
+            $this->input
+        );
+    }
+
+    public function testConsoleTransformsArrayToString()
+    {
+        $this->console->arrayToString([
+            'KEY' => 'value'
+        ]);
+
+        $this->assertEquals(
+            "KEY: value",
+            $this->console->output
+        );
+    }
+}
