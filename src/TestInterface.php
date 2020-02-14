28c4ec3fa51754bed71d82f7093fb423e6cc689f
Facu <subiabrewd@gmail.com>
2020-02-14 10:05:29 +0100

    Define test case interface

---
+++ src/TestInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PHPMetro;
+
+/**
+ * Defines the *Test interface
+ * @package PHPMetro
+ * @author https://gitlab.com/subiabre
+ * @license MIT
+ */
+interface TestInterface
+{
+    public function setUp(): void;
+}
