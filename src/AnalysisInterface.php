9c087903b0e106e075cbdc12c68dad329560b2d0
Facu <subiabrewd@gmail.com>
2020-02-14 12:44:30 +0100

    Update Interface name

--- src/AnalysisInterface.php
+++ src/AnalysisInterface.php
@@ -3,12 +3,12 @@
 namespace PHPMetro;
 
 /**
- * Defines the *Test interface
+ * Defines the Analysis interface
  * @package PHPMetro
  * @author https://gitlab.com/subiabre
  * @license MIT
  */
-interface TestInterface
+interface AnalysisInterface
 {
     public function setUp(): void;
 }
