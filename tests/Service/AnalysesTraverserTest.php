3c49fe2453f2ac5a65164c9bb99964182c8a6ec6
Facu <subiabrewd@gmail.com>
2020-03-01 13:38:24 +0100

    Define analyses traverser service tests

---
+++ tests/Service/AnalysesTraverserTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class AnalysesTraverserTest extends TestCase
+{
+    public $traverser;
+
+    public function setUp(): void
+    {
+        $this->traverser = new AnalysesTraverser();
+    }
+
+    public function testLocatesDirectory()
+    {
+        $this->traverser->setDirectory('tests');
+
+        $directory = $this->traverser->getDirectory();
+
+        $this->assertIsString($directory);
+        $this->assertDirectoryExists($directory);
+    }
+
+    public function testGetClassesReturnsArray()
+    {
+        $this->traverser->setDirectory('tests');
+
+        $tests = $this->traverser->getClasses();
+
+        $this->assertIsArray($tests);
+        $this->assertNotEmpty($tests);
+    }
+
+    public function testGetClassesWithSuffix()
+    {
+        $this->traverser->setDirectory('tests');
+        $this->traverser->setSuffix('NonExistentSuffix');
+
+        $tests = $this->traverser->getClasses();
+
+        $this->assertIsArray($tests);
+        $this->assertEmpty($tests);
+    }
+
+    public function testGetClassesWithNamespaces()
+    {
+        $this->traverser->setDirectory('tests');
+        $this->traverser->setNamespace('PHPMetro\Tests');
+
+        $tests = $this->traverser->getClasses();
+
+        $this->assertIsArray($tests);
+        $this->assertNotEmpty($tests);
+
+        $this->assertArraySubset(
+            ['tests/Service/AnalysesTraverserTest.php' => 'PHPMetro\Tests\Service\AnalysesTraverserTest'],
+            $tests  
+        );
+    }
+}
