211189c0a80bc518c8a389cb1b26bfe14ff28156
Facu <subiabrewd@gmail.com>
2020-03-01 16:00:58 +0100

    Add ConfigFinder

--- src/Service/ConfigFinder.php
+++ src/Service/ConfigFinder.php
@@ -2,6 +2,8 @@
 
 namespace PHPMetro\Service;
 
+use PHPMetro\Component\Config;
+
 /**
  * Finds and loads the configuration file of PHPMetro
  * @package PHPMetro
@@ -9,5 +11,37 @@ namespace PHPMetro\Service;
  */
 class ConfigFinder
 {
-    
+    public $rootDir;
+
+    public function __construct()
+    {
+        $traverser = new AnalysesTraverser();
+        $this->rootDir = $traverser->rootDir;
+    }
+
+    /**
+     * Load the configuration file into an object
+     * @return object
+     */
+    public function load(): Config
+    {
+        $file = $this->rootDir . '/phpmetro.xml';
+
+        $configs = [
+            $file . '.local',
+            $file . '.dist',
+            $file
+        ];
+
+        foreach ($configs as $config)
+        {
+            if (\file_exists($config)) {
+                $location = $config;
+
+                break;
+            }
+        }
+
+        return new Config($location);
+    }
 }
