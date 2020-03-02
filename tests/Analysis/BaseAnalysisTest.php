114abf1988ad8ca21994907cfb2eabab541b6912
Facu <subiabrewd@gmail.com>
2020-03-02 01:01:41 +0100

    Define Base Analysis tests

---
+++ tests/Analysis/BaseAnalysisTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PHPMetro\Analysis;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseAnalysisTest extends TestCase
+{
+    public $case;
+
+    public function setUp(): void
+    {
+        $this->case = new BaseAnalysis();
+    }
+
+    public function testAddSampleFlagsSettingUp()
+    {
+        $this->assertFalse($this->case->isSettingUp);
+
+        $this->case->addSample('Test', 1000, function(){
+            $this->assertTrue($this->case->isSettingUp);
+        });
+    }
+
+    public function testGetsAllTestsAsAnArray()
+    {
+        $tests = $this->case->getAllTests();
+
+        $this->assertIsArray($tests);
+        $this->assertEmpty($tests);
+    }
+}
