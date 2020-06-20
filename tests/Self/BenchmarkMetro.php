7fe25e72962260da0d62c55be8f3e48830b260e3
Facu <subiabrewd@gmail.com>
2020-06-20 18:09:46 +0200

    Add benchmark metro

---
+++ tests/Self/BenchmarkMetro.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PHPMetro\Tests\Self;
+
+use PHPMetro\Analysis\AnalysisCase;
+
+class BenchmarkMetro extends AnalysisCase
+{
+    public function setUp(): void
+    {
+        $this->addSample('times', 10000, function() {
+            $start = \microtime(true);
+            
+            \random_int(0, 100);
+
+            $end = \microtime(true);
+
+            return [$start, $end];
+        });
+    }
+
+    public function testTotalTime()
+    {
+        $times = $this->getSample('times');
+        $total = 0;
+
+        foreach ($times as $time)
+        {
+            $duration = $time[1] - $time[0];
+            $total += $duration;
+        }
+
+        return $total;
+    }
+}
